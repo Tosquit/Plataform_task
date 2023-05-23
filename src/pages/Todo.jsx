4e749a7e253c4611be0f7e11628661cc58adf995
Tosqui <63412765+Tosquit@users.noreply.github.com>
2023-05-23 08:05:21 +0000

    enter
    Signed-off-by: Tosqui <63412765+Tosquit@users.noreply.github.com>

---
+++ src/pages/Todo.jsx
@@ -0,0 +1,35 @@
+---
+import './todo.css'
+---
+const fakeTodos=[
+  {what:"Study",done:false}
+];
+const TodoItem=({todo})=> <li className='todo-item'>
+  <input type="checkbox" checked={todo.done}/>
+  <span className={"what" + (todo.done ? " done ": "")}>
+  {todo.what}
+  </span>
+</li>
+
+function TodoList(){
+  const [todos,setTodos]= useState(fakeTodos);
+
+  const addTodo = (event)=>{
+    event.preventDefault();
+  }
+  return(
+    <div className="todo-list">
+      < form onSubmit={addTodo}>
+          <input tyoe="text"/>
+          <button>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo)=>(
+          <TodoItem todo={todo}/>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default TodoList;
